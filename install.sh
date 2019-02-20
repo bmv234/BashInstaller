16888cd24d604f16272676e7f171bd76aa47448d
bmv234 <43771783+bmv234@users.noreply.github.com>
2019-02-19 22:30:35 -0600

    Updated

--- install.sh
+++ install.sh
@@ -27,8 +27,8 @@ sh get-docker.sh
 sudo usermod -aG docker $USER
 
 # Install Docker Compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose \
-chmod +x /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 # Install InfluxDB Docker Container
 sudo docker run -d -p 8086:8086 influxdb
