f3ee65b87e0f9b3a58c7d409322fe8d5a2926ee7
bmv234 <43771783+bmv234@users.noreply.github.com>
2019-02-19 21:06:29 -0600

    Added Docker Compose Install Script

--- install.sh
+++ install.sh
@@ -22,3 +22,7 @@ echo " "
 # Install Docker
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
+
+# Install Docker Compose
+curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
