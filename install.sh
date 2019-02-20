f87c0f43e6224a07371d6cc5c1463bc130d4adf9
bmv234 <43771783+bmv234@users.noreply.github.com>
2019-02-19 21:31:43 -0600

    Sudo

--- install.sh
+++ install.sh
@@ -20,15 +20,15 @@ echo "*********************************************************************"
 echo " "
 
 # Install Docker
-curl -fsSL https://get.docker.com -o get-docker.sh
+sudo curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 
 # Install Docker Compose
-curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 # Install InfluxDB Docker Container
-docker run --name=influxdb -d -p 8086:8086 -p 2003:2003 influxdb \
+sudo docker run --name=influxdb -d -p 8086:8086 -p 2003:2003 influxdb \
     -e INFLUXDB_GRAPHITE_ENABLED=true \
     influxdb
 
